8ae328e44d805c6da536081cbfd92af356259416
aoolukitibi <aoolukitibi@gmail.com>
2015-11-02 11:26:28 -0500

    Initial Commit

---
+++ Convention App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Convention App
+//
+//  Created by Anthony Olukitibi on 7/16/15.
+//  Copyright (c) 2015 Anthony Olukitibi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Convention App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Convention App
+//
+//  Created by Anthony Olukitibi on 7/16/15.
+//  Copyright (c) 2015 Anthony Olukitibi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
